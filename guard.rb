4533f434c61646e28ea33ec8601ee7d87a22880c
Ricardo Mejía <mejiaro@gmail.com>
2018-05-05 17:18:35 -0600

    add method to Guard

--- guard.rb
+++ guard.rb
@@ -1,2 +1,5 @@
 class Guard
+	def can_participate_in_lottery?(age:)
+		age > 17
+	end
 end
--- guard_spec.rb
+++ guard_spec.rb
@@ -3,6 +3,13 @@ require './guard'
 RSpec.describe Guard do
 	describe "#can_participate_in_lottery?" do
 		it 'returns true if user age > 17' do
+			service = Guard.new
+			expect(service.can_participate_in_lottery?(age: 20)).to eq(true)
+		end
+
+		it 'returns false if user age < 18' do
+			service = Guard.new
+			expect(service.can_participate_in_lottery?(age: 17)).to eq(false)
 		end
 	end
 end
